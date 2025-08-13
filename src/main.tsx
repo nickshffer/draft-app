df71e24b0352968656b9f637f8206efe9df88313
Nick Shaffer <nickshffer@gmail.com>
2025-08-13 15:01:26 -0700

    bugfix

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.PROD ? '/draft-app' : ''}>
       <Routes>
         <Route path="/host" element={<App isHost={true} />} />
         <Route path="/" element={<App isHost={false} />} />
